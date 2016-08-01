8455dd0a70827a00ee62f0466af13b9e873c4970
yugege <474700224@qq.com>
2016-08-01 17:58:14 +0800

    底部事件

---
+++ src/cn/ucai/fuliCenter/activity/FulicenterMainActivity.java
@@ -0,0 +1,18 @@
+package cn.ucai.fuliCenter.activity;
+
+import android.os.Bundle;
+
+import com.easemob.chatuidemo.R;
+
+/**
+ * Created by Administrator on 2016/8/1.
+ */
+public class FulicenterMainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fulicenter_main);
+
+    }
+}
