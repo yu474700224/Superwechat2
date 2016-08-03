637cb8f3b9356b77d6e5f9381ac8cb6feac2bb08
yugege <474700224@qq.com>
2016-08-03 22:27:30 +0800

    获取商品详情并显示

---
+++ src/cn/ucai/fuliCenter/activity/GoodDetailsActivity.java
@@ -0,0 +1,23 @@
+package cn.ucai.fuliCenter.activity;
+
+import android.os.Bundle;
+
+import com.easemob.chatuidemo.R;
+
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+public class GoodDetailsActivity extends BaseActivity {
+    private final static String TAG = GoodDetailsActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.good_info);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
